94ceea13c5adfbb00df23c4342ed8c8de5a32c88
Paulo Alexandre <paulofore@gmail.com>
2019-12-08 12:54:31 -0300

    proxy

--- src/coletapreco/app/NotificadorSender.java
+++ src/coletapreco/app/NotificadorSender.java
@@ -64,8 +64,8 @@ public class NotificadorSender {
              }
          };
          Authenticator.setDefault(authenticator);    
-         HttpURLConnection conn = (HttpURLConnection) new URL("https://fcm.googleapis.com/fcm/send").openConnection(proxy);
-         //HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+         //HttpURLConnection conn = (HttpURLConnection) new URL("https://fcm.googleapis.com/fcm/send").openConnection(proxy);
+         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
          
          conn.setRequestProperty("Authorization", "key=" + apiKey);
          conn.setRequestProperty("Content-Type", "application/json");
